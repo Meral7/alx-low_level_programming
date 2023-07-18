8270d72e87c1f054894102932d3ed27ad26a996c
Meral7 <mera722002@gmail.com>
2023-07-18 15:22:37 -0700

    new

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -5,9 +5,9 @@
 */
 void print_alphabet_x10(void)
 {
-int j = 0;
-char m = 'a';
-for (j = 0; j < 10; j++)
+int i = 0;
+char a = 'a';
+for (i = 0; i < 10; i++)
 {
 for (a = 'a'; a <= 'z'; a++)
 _putchar(a);

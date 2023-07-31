15558dfd72840fcae237a90efee485e6a096ccba
Meral7 <mera722002@gmail.com>
2023-07-31 13:42:31 -0700

    pointer part4

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,22 +1,21 @@
 #include "main.h"
 /**
- * _memset - fill a block of memory
- * @s: starting address of memoryy
- * @b: constant byte
- * @n:  bytes of memory to be changed
+ * _memset - fill a blocck of memory
+ * @s: starting addres of memory
+ * @b: the desired valuee
+ * @n: member of bytes too be changed
  *
- * Return: pointer to memmory
+ * Return: Always value
  */
 char *_memset(char *s, char b, unsigned int n)
 {
-        unsigned int i = 0;
+	unsigned int i = 0;
 
-        while (i < n)
-        {
-                *(s + i) = b;
-                i++;
-        }
-        return (s);
+	while (i < n)
+	{
+		*(s + i) = b;
+		i++;
+	}
+	return (s);
 }
-
 
